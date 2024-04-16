63749389139330f38fe307bc4944ec224e6c45e6
PedroSousa025 <167201267+PedroSousa025@users.noreply.github.com>
2024-04-16 12:04:06 -0300

    Update testando_streamlit.py

--- testando_streamlit.py
+++ testando_streamlit.py
@@ -7,7 +7,7 @@ st.write("""
 """)
 
 number = st.slider("Pick a number", 0, 100)
-multiplicador =st.select_slider('Slide to select', options=[1,'2'])
+multiplicador =st.select_slider('Slide to select', options=[1,2,3,4,5,6,7,8,9,10])
 final = number * multiplicador
 
 st.write("o número final foi: {}" .format(final))
