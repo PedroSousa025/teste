65332234f9078349fd5848c594bd5add0751eabb
PedroSousa025 <167201267+PedroSousa025@users.noreply.github.com>
2024-04-16 13:08:13 -0300

    Update testando_streamlit.py

--- testando_streamlit.py
+++ testando_streamlit.py
@@ -9,6 +9,7 @@ st.write("""
 number = st.slider("Escolha um número: ", 0, 100)
 multiplicador =st.select_slider('Escolha o multiplicador:', options=[1,2,3,4,5,6,7,8,9,10])
 final = number * multiplicador
+st.camera_input("一二三,茄子!")
 
 st.write("o número final foi: {}" .format(final))
 
