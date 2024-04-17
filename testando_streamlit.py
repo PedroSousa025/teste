b7a1124ed1f4c2ceb8ceff5d464fbd73dc9f0735
PedroSousa025 <167201267+PedroSousa025@users.noreply.github.com>
2024-04-17 16:26:53 -0300

    Update testando_streamlit.py

--- testando_streamlit.py
+++ testando_streamlit.py
@@ -13,7 +13,9 @@ number = st.slider("Escolha um número: ", 0, 100)
 multiplicador =st.select_slider('Escolha o multiplicador:', options=[1,2,3,4,5,6,7,8,9,10])
 final = number * multiplicador
 
-st.color_picker("selecione uma cor: ")
+st.toast("não faço a minima ideia do que isso faz")
+
+cor = st.color_picker("selecione uma cor: ")
 st.write("o número final foi: {}" .format(final))
 
 
