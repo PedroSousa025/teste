5e9cff55814ce1a37c78bf138e690cfd1f2a3b4c
PedroSousa025 <167201267+PedroSousa025@users.noreply.github.com>
2024-04-17 16:34:20 -0300

    Update testando_streamlit.py

--- testando_streamlit.py
+++ testando_streamlit.py
@@ -20,6 +20,7 @@ final = number * multiplicador
 st.toast("não faço a minima ideia do que isso faz")
 
 cor = st.color_picker("selecione uma cor: ")
-st.write("o número final foi: :blue[{}]" .format(bluefinal))
+st.write("o número final foi: {}" .format(bluefinal))
+st.write("testando :blue[cores]")
 
 
